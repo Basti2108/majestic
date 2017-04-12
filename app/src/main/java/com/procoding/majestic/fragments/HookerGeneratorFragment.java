1893b5059930769fb883a0b29a477328cde6b47e
Basti <sebastian_jaekl@web.de>
2017-04-13 00:02:13 +0200

    Version 0.1.2 worked on HookerGenerator -> preparations for the output & etc added a new String to StringArray in strings.xml

--- app/src/main/java/com/procoding/majestic/fragments/HookerGeneratorFragment.java
+++ app/src/main/java/com/procoding/majestic/fragments/HookerGeneratorFragment.java
@@ -6,7 +6,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -19,6 +22,9 @@ public class HookerGeneratorFragment extends Fragment {
     int randomNumber;
     private Spinner hookerSpinner;
     private View myFragmentView;
+    private Button suggestionBtn;
+    private TextView hookerSuggestion;
+
 
 
 
@@ -36,6 +42,8 @@ public class HookerGeneratorFragment extends Fragment {
 
 
         hookerSpinner = (Spinner) myFragmentView.findViewById(R.id.spinner_hooker);
+        suggestionBtn = (Button) myFragmentView.findViewById(R.id.button_random_hooker);
+        hookerSuggestion = (TextView) myFragmentView.findViewById(R.id.textView_Hooker);
 
         //Create an ArrayAdapter unsing the StringArray and a default Spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getActivity() , R.array.hooker_tastes, android.R.layout.simple_spinner_item);
@@ -44,7 +52,49 @@ public class HookerGeneratorFragment extends Fragment {
         //apply the Adapter to the spinner
         hookerSpinner.setAdapter(adapter);
 
+
+
+        //the Button which suggest a hooker taste after you choose one of the elements in the Spinner!
+        //The TextView will later show the Suggested Taste!
+        suggestionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //First Item in the Spinner = Fruchtig
+                if(hookerSpinner.getSelectedItemId() == 0 ){
+                    //TODO show one of the Tastes in FruitySection
+                    hookerSuggestion.setText("Fruchtig");
+                }else
+                    //Second Item in the Spinner = Frisch!
+                    if(hookerSpinner.getSelectedItemId() == 1){
+                        //TODO show one of the Tastes in the FreshSection
+                        hookerSuggestion.setText("Frisch");
+                    }else
+                        //Third Item in the Spinner = Fruchtig/Frisch
+                        if(hookerSpinner.getSelectedItemId() == 2){
+                            //TODO show one of the Tastes in the Fruity/FreshSection
+                        hookerSuggestion.setText("Fruchtig/Frisch");
+                    }else
+                        //Fourth Item in the Spinner = Cremig
+                        if(hookerSpinner.getSelectedItemId() == 3){
+                            //TODO show one of the Tastes in the CreamySection
+                        hookerSuggestion.setText("Cremig");
+
+                    }else
+                        //Fifth Item in the Spinner = Alles
+                        if(hookerSpinner.getSelectedItemId() == 4){
+                            //TODO show one of the Tastes in the EverythinSection
+                            hookerSuggestion.setText("Alles");
+                        }
+            }
+
+        });
+
+
         return myFragmentView;
+
+        //Returns the adapter currently associated with this widget.
+
     }
 
 
