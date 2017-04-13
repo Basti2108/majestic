9615c60d4aa91fb38d74ff0c95d42247f740d843
Basti <sebastian_jaekl@web.de>
2017-04-13 16:24:02 +0200

    0.2 Added google-services in build-grade app and build-grade project hopefully a big step! deleted google maps and added another part of google firebase

--- app/src/main/java/com/procoding/majestic/fragments/ApproachFragment.java
+++ app/src/main/java/com/procoding/majestic/fragments/ApproachFragment.java
@@ -13,13 +13,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
+
 import com.procoding.root.majestic.R;
 
 
@@ -36,7 +30,7 @@ public class ApproachFragment extends Fragment {
     private Typeface CopperplateGothicBoldRegular;
 
     private Button buttonNavigator;
-    private MapView mapView;
+    //private MapView mapView;
 
 
     public ApproachFragment() {
@@ -55,42 +49,17 @@ public class ApproachFragment extends Fragment {
         CopperplateGothicBoldRegular = Typeface.createFromAsset(getActivity().getAssets(), "fonts/CopperplateGothicBoldRegular.ttf");
         CopperplateGothicLightRegular = Typeface.createFromAsset(getActivity().getAssets(), "fonts/CopperplateGothicLightRegular.ttf");
 
-        mapView = (MapView) myFragmentView.findViewById(R.id.mapView);
+        //mapView = (MapView) myFragmentView.findViewById(R.id.mapView);
         textName = (TextView) myFragmentView.findViewById(R.id.textViewName);
         textAdress = (TextView) myFragmentView.findViewById(R.id.textViewAdress);
         textCity = (TextView) myFragmentView.findViewById(R.id.textViewAdress);
 
         buttonNavigator = (Button) myFragmentView.findViewById(R.id.buttonNavigation);
 
-        //TODO Get GoogleMaps to work inside of the app
-        mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(GoogleMap googleMap) {
 
 
-                if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return;
-                }
-                googleMap.setMyLocationEnabled(true);
-                googleMap.getUiSettings().setZoomControlsEnabled(true);
-                LatLngBounds.Builder builder = new LatLngBounds.Builder();
-                builder.include(new LatLng(55.854049, 13.661331));
-                LatLngBounds bounds =  builder.build();
-                int padding = 0;
-                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, padding);
-                googleMap.moveCamera(cameraUpdate);
 
 
-            }
-        });
 
 
 
